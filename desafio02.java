ac83ce661e6979cab43125c60423db6132916d81
MayconL27 <mayconleandro2008@gmail.com>
2022-05-18 12:12:34 -0300

    Desafio 02

---
+++ desafio02.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class desafio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int multiplosDe2 = 0, multiplosDe3 = 0, multiplosDe4 = 0, multiplosDe5 = 0;
+
+        for (int i = 0; i < n; i++) {
+            int li = sc.nextInt();
+            if (li % 2 == 0) {
+                multiplosDe2++;
+            }
+            if (li % 3 == 0) {
+                multiplosDe3++;
+            }
+            //TODO: Complete a solução criando as condições e incrementos para os múltiplos de 4 e 5.
+        }
+             if (li % 4 == 0) {
+             multiplosDe4++;
+            }
+             if (li % 5 == 0) {
+             multiplosDe5++;
+            }
+
+        System.out.printf("%d Multiplo(s) de 2\n", multiplosDe2);
+        System.out.printf("%d Multiplo(s) de 3\n", multiplosDe3);
+        System.out.printf("%d Multiplo(s) de 4\n", multiplosDe4);
+        System.out.printf("%d Multiplo(s) de 5\n", multiplosDe5);
+
+    }    
+}
